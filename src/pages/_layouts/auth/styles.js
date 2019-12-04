9b44e4862aadcebcbf9e1c9cd22c25a9b7f11adf
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-04 00:09:51 -0300

    feat: add auth and default layouts

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import colors from '../../../styles/colors';
+
+export const Wrapper = styled.div`
+  height: 100px;
+  background: ${colors.notloggedBg};
+  color: ${colors.white};
+`;
---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  primary: '#EE4D64',
+  white: '#FFFFFF',
+  black: '#000000',
+  grayDark: '#444444',
+  gray: '#666666',
+  grayLight: '#999999',
+  lines: '#DDDDDD',
+  loggedBg: '#F5F5F5',
+  notloggedBg: '#EE4D64',
+  buttonPrimary: '#EE4D64',
+  buttonSecondary: '#CCCCCC',
+};
