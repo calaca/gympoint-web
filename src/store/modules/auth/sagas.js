4ef83d66d9d8d122758c9b4625552c52b98e1173
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-05 14:11:10 -0300

    feat: add toast messages

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import constants from './constants';
 import { signInSuccess, signFailure } from './actions';
 
@@ -20,6 +21,7 @@ export function* signIn({ payload }) {
 
     history.push('/students');
   } catch (err) {
+    toast.error('Falha na autenticação. Por favor, verifique seus dados.');
     yield put(signFailure());
   }
 }
