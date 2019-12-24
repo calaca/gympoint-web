5cb0d0ee2328d99e6bed50033b23cd4aaad3746d
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-24 16:19:41 -0300

    feat: connect app with redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
+
+import store from './store';
+
 import theme from './styles/theme';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
