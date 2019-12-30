23b8094215fc61c95510d96de5930afe6cbe13f3
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-30 17:52:40 -0300

    fix: remove unused files

--- src/utils/currencyFormatter.js
+++
@@ -1,7 +0,0 @@
-export default amount => {
-  const regex = /\b(\d+)(\d{2})\b/;
-  const amountStr = amount.toString();
-  const subst = '$1,$2';
-
-  return amountStr.replace(regex, subst);
-};
--- src/utils/formatMetricToInteger.js
+++
@@ -1,5 +0,0 @@
-export default str =>
-  str
-    .match(/\d+/g)
-    .join()
-    .replace(',', '');
