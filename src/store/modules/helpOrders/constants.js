8cfaed19efd9c651a8e0ed4e9c590dff6214b3b0
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-30 17:55:55 -0300

    fix: remove unused files

--- src/store/modules/helpOrders/constants.js
+++
@@ -1,7 +0,0 @@
-export default {
-  helpOrdersLoadRequest: '@helpOrders/LOAD_REQUEST',
-  helpOrdersLoadSuccess: '@helpOrders/LOAD_SUCCESS',
-  helpOrdersAnswerRequest: '@helpOrders/ANSWER_REQUEST',
-  helpOrdersAnswerSuccess: '@helpOrders/ANSWER_SUCCESS',
-  helpOrdersFailure: '@helpOrders/HELP_ORDER_FAILURE',
-};
